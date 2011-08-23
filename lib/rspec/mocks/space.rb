8df10c3990860f031e03b2d020555fd62cc78805
Schneems <richard.schneeman@gmail.com>
2011-08-22 21:26:27 -0400

    initial docs space.rb

--- lib/rspec/mocks/space.rb
+++ lib/rspec/mocks/space.rb
@@ -1,16 +1,23 @@
 module RSpec
   module Mocks
+    # Used to encapsulate and verify mocks as well as reset mocks
     class Space
+      # Adds an object to the mock array if it previously did not exist
       def add(obj)
         mocks << obj unless mocks.detect {|m| m.equal? obj}
       end
 
+      # calls rspec_verify on each object in the mocks array
+      # @see RSpec::Mocks::Methods#rspec_verify
       def verify_all
         mocks.each do |mock|
           mock.rspec_verify
         end
       end
 
+      # calls rspec_reset on each object in the mocks array
+      # then removes all elements from the mocks array
+      # @see RSpec::Mocks::Methods#rspec_reset
       def reset_all
         mocks.each do |mock|
           mock.rspec_reset
@@ -19,7 +26,7 @@ module RSpec
       end
       
     private
-    
+      # holds all mock objects
       def mocks
         @mocks ||= []
       end
